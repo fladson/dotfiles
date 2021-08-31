e3af14c6b9507079cbc791e3a5dd37e7003efdd3
Fladson Gomes <fladsonthiago@gmail.com>
2021-08-31 09:51:31 +0200

    nvim: add dark and light colors

---
+++ nvim/.config/nvim/lua/colors/dark.lua
@@ -0,0 +1,32 @@
+local M = {}
+M.colors = {
+  red = '#F9867B',
+  orange = '#CD9731',
+  yellow = '#FDCE68',
+  green = '#A3EEA0',
+  cyan = '#6796E6',
+  blue = '#7BE2F9',
+  purple = '#E19EF5',
+  tomato = '#F44747',
+  white = '#FFFFFF',
+
+  background = '#212835',
+  dark_background = '#1B212C',
+
+  border = '#3E4451',
+  comment = '#5C6370',
+  lineNumber = '#495162',
+  scrollbar = '#4E5666',
+  cursorLine = '#2C313A',
+  indent = '#383C44',
+}
+
+vim.cmd('set background=dark')
+-- tomorrow night
+return {
+  base00 = '#1d1f21', base01 = '#282a2e', base02 = '#373b41', base03 = '#969896',
+  base04 = '#b4b7b4', base05 = '#c5c8c6', base06 = '#e0e0e0', base07 = '#ffffff',
+  base08 = '#cc6666', base09 = '#de935f', base0A = '#f0c674', base0B = '#b5bd68',
+  base0C = '#8abeb7', base0D = '#81a2be', base0E = '#b294bb', base0F = '#a3685a'
+}
+
---
+++ nvim/.config/nvim/lua/colors/light.lua
@@ -0,0 +1,30 @@
+local M = {}
+M.colors = {
+  red = '#F9867B',
+  orange = '#CD9731',
+  yellow = '#FDCE68',
+  green = '#A3EEA0',
+  cyan = '#6796E6',
+  blue = '#7BE2F9',
+  purple = '#E19EF5',
+  tomato = '#F44747',
+  white = '#FFFFFF',
+
+  background = '#212835',
+  dark_background = '#1B212C',
+
+  border = '#3E4451',
+  comment = '#5C6370',
+  lineNumber = '#495162',
+  scrollbar = '#4E5666',
+  cursorLine = '#2C313A',
+  indent = '#383C44',
+}
+
+vim.cmd('set background=light')
+return {
+    base00 = '#eeeeee', base01 = '#af0000', base02 = '#008700', base03 = '#5f8700',
+    base04 = '#0087af', base05 = '#444444', base06 = '#005f87', base07 = '#878787',
+    base08 = '#bcbcbc', base09 = '#d70000', base0A = '#d70087', base0B = '#8700af',
+    base0C = '#d75f00', base0D = '#d75f00', base0E = '#005faf', base0F = '#005f87'
+}
