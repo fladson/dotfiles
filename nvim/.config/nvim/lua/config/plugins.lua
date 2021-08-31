b0dfefca96108f7ff1d5b0338d9a560f211b5029
Fladson Gomes <fladsonthiago@gmail.com>
2021-08-31 09:26:36 +0200

    nvim: add treesitter playground

--- nvim/.config/nvim/lua/config/plugins.lua
+++ nvim/.config/nvim/lua/config/plugins.lua
@@ -9,6 +9,7 @@ return require("packer").startup(function()
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate"
   }
+  use "nvim-treesitter/playground"
   -- use "lukas-reineke/indent-blankline.nvim"
   use "neovim/nvim-lspconfig"
   use "hrsh7th/nvim-compe"
